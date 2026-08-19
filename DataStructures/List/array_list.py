edc26a9a9b86fa25a53d829ba7beee8f41fb0383
Dayanna Camargo <sd.camargo@uniandes.edu.co>
2026-08-18 19:42:47 -0500

    add first

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -4,3 +4,8 @@ def new_list():
         'size': 0,
             }
     return newlist
+
+def add_first(my_list, element):
+    my_list['elements'].insert(0, element)
+    my_list['size'] += 1
+    return my_list
--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -5,3 +5,8 @@ def new_list():
         "size": 0,
             }
     return newlist  
+
+def add_first(my_list, element):
+    my_list['elements'].insert(0, element)
+    my_list['size'] += 1
+    return array_list
