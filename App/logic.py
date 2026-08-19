eafb55280d3fb5d7ebeba7a56f67480dda1aed18
Efrain-Tellez <e.tellezb@uniandes.edu.co>
2026-08-18 19:14:13 -0500

    paso 5 solucion

--- App/logic.py
+++ App/logic.py
@@ -28,7 +28,9 @@ import csv
 import os
 import time
 
+
 # TODO Importar la librería para el manejo de listas
+from DataStructures.List import array_list as lt
 
 data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
 
