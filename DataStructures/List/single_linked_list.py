e4d71aa4c0c8dfb69bc4a4a8522ebc29e8ddff6b
Miguelangel0712 <m.villalbam@uniandes.edu.co>
2026-08-31 21:58:39 -0500

    Correcciones

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -34,37 +34,38 @@ def is_present(my_list, element, cmp_function):
     return count
 
 def add_first(my_list, element):
-    node = node.new_single_node(element)
-    node["next"] = my_list["first"]  
-    my_list["first"] = node
+    nuevo_nodo = node.new_single_node(element)
+    nuevo_nodo["next"] = my_list["first"]  
+    my_list["first"] = nuevo_nodo
     
     if my_list["size"] == 0:
-        my_list["last"] = node
+        my_list["last"] = nuevo_nodo
         
     my_list["size"] += 1
         
     return my_list
 
 def add_last(my_list, element):
-    node = node.new_single_node(element)
+    nuevo_nodo = node.new_single_node(element)
     
     if my_list["size"] == 0:
-        my_list["first"] = node
-        my_list["last"] = node
+        my_list["first"] = nuevo_nodo
+        my_list["last"] = nuevo_nodo
     else:
-        my_list["last"]["next"] = node
-        my_list["last"] = node
+        my_list["last"]["next"] = nuevo_nodo
+        my_list["last"] = nuevo_nodo
         
     my_list["size"] += 1
     return my_list
 
 def is_empty(my_list):
-    if my_list["size"] == 0:
+    tamaño = size(my_list)
+    if tamaño == 0:
         return True
-    else:
+    else: 
         return False
-    
 
+    
 def size(my_list):
     return my_list["size"]
 
@@ -136,6 +137,66 @@ def insert_element(my_list, element, pos):
     nodo_anterior["next"] = nodo
     my_list["size"] += 1
         
+    return my_list
+
+def delete_element(my_list, pos):
+    tamaño = size(my_list)
+    if pos < 0 or pos >= tamaño:
+        return my_list
+
+    if pos == 0:
+        remove_first(my_list)
+        return my_list
+
+    nodo_anterior = my_list["first"]
+
+    for i in range(pos - 1):
+        nodo_anterior = nodo_anterior["next"]
+
+    nodo_removido = nodo_anterior["next"]
+    nodo_anterior["next"] = nodo_removido["next"]
+
+    if pos == tamaño - 1:
+        my_list["last"] = nodo_anterior
+
+    tamaño -= 1
+
+    return my_list
+
+
+def change_info(my_list, pos, new_info):
+    tamaño = size(my_list)
+    if pos < 0 or pos >= tamaño:
+        return my_list
+
+    nodo_actual = my_list["first"]
+
+    for i in range(pos):
+        nodo_actual = nodo_actual["next"]
+
+    nodo_actual["info"] = new_info
+
+    return my_list
+
+def exchange(my_list, pos1, pos2):
+    tamaño = size(my_list)
+    if pos1 < 0 or pos1 >= tamaño:
+        return my_list
+
+    if pos2 < 0 or pos2 >= tamaño:
+        return my_list
+
+    nodo1 = my_list["first"]
+    nodo2 = my_list["first"]
+
+    for i in range(pos1):
+        nodo1 = nodo1["next"]
+
+    for i in range(pos2):
+        nodo2 = nodo2["next"]
+
+    nodo1["info"], nodo2["info"] = nodo2["info"], nodo1["info"]
+
     return my_list
   
 
